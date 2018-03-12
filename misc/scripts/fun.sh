2da6ccd3896fdacedd6556fef239d50a6967fc20
Santosh Astagi <santosh.astagi@rallyhealth.com>
2018-03-12 16:14:05 -0700

    Get travis build number

--- misc/scripts/fun.sh
+++ misc/scripts/fun.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 
+echo ${TRAVIS_BUILD_NUMBER}
 
-test_report_directory_path_all_devices=$(gsutil ls gs://coverage_reports/jenkins_build_number/0/* | grep -e "/$")
-test_report_directory_path_single_device=(${testreport_directory_path_all_devices})
-download_path_for_coverage_report=${testreport_directory_path_single_device[0]}coverage.ec
-echo "Downloading coverage reports from from: ${download_path_for_coverage_report}"
-gsutil cp ${path} ./app/coverage_reports/0.ec
+#test_report_directory_path_all_devices=$(gsutil ls gs://coverage_reports/jenkins_build_number/0/* | grep -e "/$")
+#test_report_directory_path_single_device=(${testreport_directory_path_all_devices})
+#download_path_for_coverage_report=${testreport_directory_path_single_device[0]}coverage.ec
+#echo "Downloading coverage reports from from: ${download_path_for_coverage_report}"
+#gsutil cp ${path} ./app/coverage_reports/0.ec
