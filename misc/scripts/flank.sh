2eb83327b6df0b80b008b96145bff7b8e2242dce
Santosh Astagi <santosh.astagi@rallyhealth.com>
2018-03-12 17:32:18 -0700

    Final working draft

--- misc/scripts/flank.sh
+++ misc/scripts/flank.sh
@@ -2,12 +2,12 @@
 
 DIR_NAME=jenkins_build_number
 
-## Fetch flankt.test
-gsutil -q stat gs://coverage_reports/flank.tests/
+## Fetch flank.test
+gsutil -q stat gs://coverage_reports/flank.tests
 
 return_value=$?
 
-if [ ${return_value} != 0 ]; then
+if [ ${return_value} == 0 ]; then
     echo "flank.tests found"
     gsutil cp gs://coverage_reports/flank.tests .
 else
@@ -20,21 +20,27 @@ echo "gcloud-bucket: coverage_reports/${DIR_NAME}" >> config.properties
 rm config.properties.bak
 
 ## Remove old coverage reports
-rm -r ./app/coverage_reports/
+rm -rf app/coverage_reports/*.exec
 
 gsutil cp config.properties  gs://coverage_reports/${DIR_NAME}/
 
 java -jar Flank-2.0.1.jar -a app/build/outputs/apk/debug/app-debug.apk -t app/build/outputs/apk/androidTest/debug/app-debug-androidTest.apk
 
 REPORT_COUNT=$(gsutil ls gs://coverage_reports/${DIR_NAME}/ | wc -l)
-
 for number in `seq 0 ${REPORT_COUNT}`
 do
-    test_report_directory_path_all_devices=$(gsutil ls gs://coverage_reports/jenkins_build_number/${number}/* | grep -e "/$")
-    test_report_directory_path_single_device=(${test_report_directory_path_all_devices})
-    download_path_for_coverage_report=${test_report_directory_path_single_device[0]}coverage.ec
-    echo "Downloading coverage reports from from: ${download_path_for_coverage_report}"
-    gsutil cp ${download_path_for_coverage_report} ./app/coverage_reports/${number}.exec
+    gsutil -q stat gs://coverage_reports/jenkins_build_number/${number}/*
+
+    return_value=$?
+
+    if [ ${return_value} == 0 ]; then
+        test_report_directory_path_all_devices=$(gsutil ls gs://coverage_reports/jenkins_build_number/${number}/* | grep -e "/$")
+        test_report_directory_path_single_device=(${test_report_directory_path_all_devices})
+        download_path_for_coverage_report=${test_report_directory_path_single_device[0]}coverage.ec
+        echo "Downloading coverage reports from from: ${download_path_for_coverage_report}"
+        gsutil cp ${download_path_for_coverage_report} ./app/coverage_reports/${number}.exec
+    fi
+
 done
 
 gsutil mv gs://coverage_reports/${DIR_NAME}/flank.tests gs://coverage_reports/
